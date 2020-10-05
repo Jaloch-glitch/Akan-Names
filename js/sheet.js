c5244c54f2055217adafddcc0c8e9f1426e2f869
Jaloch glitch <jalochfelix@gmail.com>
2020-10-05 10:02:17 +0300

    add function submit and add arrays for male and female akan names and the days of the week

---
+++ js/sheet.js
@@ -0,0 +1,5 @@
+function submit() {
+    const daysOfTheWeek = ["Sunday", "Monday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const maleNames = ["kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    const femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"]
+    
