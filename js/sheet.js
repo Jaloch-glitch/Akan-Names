7c9952b56201fc7bd989a7084a3831e649b8b151
Jaloch glitch <jalochfelix@gmail.com>
2020-10-05 10:05:12 +0300

    add variables month, date, name and year and give intructions to the js to get the elements by their respective id, perseint and add value to each component

--- js/sheet.js
+++ js/sheet.js
@@ -2,4 +2,9 @@ function submit() {
     const daysOfTheWeek = ["Sunday", "Monday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const maleNames = ["kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     const femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"]
+
+    const month = parseInt(document.getElementById("month").value)
+    const name = (document.getElementById("name").value)
+    const year =parseInt(document.getElementById("year").value)
+    const day = parseInt(document.getElementById("day").value)
     
